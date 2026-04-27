21397159ec6f5c154254d365ada7665fb487221a
Fiodar <federal8228@gmail.com>
2026-04-27 14:31:28 +0200

    feat: add gemini model

--- frontend/src/app/(dashboard)/dashboard/tasks/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/tasks/page.tsx
@@ -1,7 +1,60 @@
-export default function TasksPage(){
+"use client";
+
+import { api } from "@/api/axios";
+import { useMutation } from "@tanstack/react-query";
+
+export default function TasksPage() {
+
+    const mutation = useMutation({
+        mutationFn: async () =>
+            await generateText(
+                "tell me a joke."
+            ),
+
+        onSuccess: (data) => {
+            console.log("Generated text:", data);
+        }
+    });
+
+    const handleGenerate = () => {
+        mutation.mutate();
+    };
+
     return (
         <div>
             <h1>tasks page</h1>
+
+            <button onClick={handleGenerate}>
+                click
+            </button>
+
+            {mutation.isPending && (
+                <p>Loading...</p>
+            )}
+
+            {mutation.data && (
+                <div>
+                    <h3>Response:</h3>
+
+                    <p>
+                        {mutation.data.data}
+                    </p>
+                </div>
+            )}
+
+            {mutation.error && (
+                <p>Error occurred</p>
+            )}
         </div>
-    )
+    );
+}
+
+async function generateText(prompt: string) {
+
+    const response = await api.post(
+        "/tasks/generate",
+        { prompt }
+    );
+
+    return response.data;
 }
